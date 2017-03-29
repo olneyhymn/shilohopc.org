b9074e2857a3e62886b66663ed61980b8a7bec8d
Tim Hopper <tdhopper@gmail.com>
2017-03-28 20:27:42 -0400

    change name

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'admin'
-SITENAME = u'Shiloh Presbyterian Church'
+SITENAME = u'Shiloh Presbyterian Church | Raleigh, NC'
 SITEURL = ''
 
 PATH = 'content'
