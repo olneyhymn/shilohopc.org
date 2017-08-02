95a4d3d6acad80b3d60bbc6a708d5a688f0734e5
Tim Hopper <tdhopper@gmail.com>
2017-08-02 14:05:27 -0400

    https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://shilohopc.org'
+SITEURL = 'https://shilohopc.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
